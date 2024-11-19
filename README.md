0a13ad1b8d2cea16cb478ce56a1586d13286193c
Nate Z <assured.toady_0n@icloud.com>
2024-11-19 15:09:33 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Uncle's Cookies & Ice Cream
+
+### This project covers:
+
+- Algorithmic thinking
+- Data structures
+- Array iteration
+- Functions
+- Modular Code
+- Flexbox
+- Wireframes
+- Git and Github
+
+**_Please read entire README before getting started_**
+
+## Goal
+
+You are on the frontend team for Uncle Matt's Cookies and Ice Cream company. It's a small startup, but they're growing fast. Your team has been tasked to build the company's new website.
+
+The business has listed out the required features of the website:
+
+## MVP Features
+
+1. A header, centered at the top of the page, that says, "Uncle's Cookies & Ice Cream"
+2. All the cookies (picture, name, and description) displayed on the page with a header that says, "Cookies"
+3. All the ice cream flavors (picture, name, and description) displayed on the page with a header that says, "Ice Cream"
+4. The cookies should be in a column on the left side of the page.
+5. The ice cream should be in a column on the right side of the page.
+6. Each individual cookie and ice cream flavor should be displayed with the image on the left and the name and description on the right. The name should be in bold and the description should be underneath the name.
+7. The headers for the cookies and the ice cream should be centered to their columns.
+
+## Stretch Goal Features
+
+These are features that the business would like you to implement if all the MVP features have been met.
+
+1. The cookies and the ice cream should be displayed in alphabetical order.
+2. Each cookie item should have a dotted baby blue border.
+3. Each ice cream flavor should have a dashed pink border.
+4. The font for the headers and the names should be the same _fun_ font.
+5. The title for the website should be in purple and a special font.
+6. The descriptions for the items should be italicized.
+
+## Tech Requirements
+
+Your tech lead for your frontend team at Uncle Matt's Cookies and Ice Cream company has specific requirements for the developmental process and has listed them below:
+
+1. You must make a wireframe before starting the project.
+2. You must modularize your code. Think SRP.
+3. You must do all your work on a feature branch. Never on `main`.
+4. You must make a new feature branch for each feature.
+5. You must adhere to the team's branch naming convention: `{initials}-{featureName}`.
+6. Your teammates **_must_** _review_ and approve your pull request before _you_ merge it into main.
+7. You must use good CSS naming conventions, preferably the BEM format.
+8. You must use Flexbox and/or CSS grid.
+
+## Set Up
+
+1.  Everyone clone the repository in your `workspace` directory with `git clone {connection string}`
+2.  Everyone immediately create their first feature branch with `git checkout -b {initials-featureName}`
---
+++ scripts/database.js
@@ -0,0 +1,87 @@
+const database = {
+  cookies: [
+    {
+      id: 1,
+      name: 'Red Velvet Cupcake',
+      img: 'https://crumbl.video/cdn-cgi/image/width=600/https://crumbl.video/b691f524-1872-41bd-a150-0d4b38bc3f3f_RedVelvetCupcake_Aerial_Tech.png',
+      description:
+        'A red velvet cookie topped with a swirl of delicious vanilla cream cheese frosting and a sprinkle of crumbly red velvet pieces.',
+    },
+    {
+      id: 2,
+      name: 'Milk Chocolate Chip',
+      img: 'https://crumbl.video/cdn-cgi/image/width=600/https://crumbl.video/b31ec433-ee3d-4ace-8758-6921d5c671c6_ChocolateChip_Aerial_Tech.png',
+      description:
+        "The classic—you can't go wrong. Thick, soft, and packed with milk chocolate chips",
+    },
+    {
+      id: 3,
+      name: 'Strawberry Shortcake',
+      img: 'https://crumbl.video/cdn-cgi/image/width=600/https://crumbl.video/81c9440d-b562-417b-9f5e-0bf2d3d4047b_StrawberryShortcake_Aerial_Tech.png',
+      description:
+        'A cakey cookie topped with silky whipped cream, house-made strawberry jam, and freshly chopped strawberries',
+    },
+    {
+      id: 4,
+      name: 'Cinnamon Frybread',
+      img: 'https://crumbl.video/cdn-cgi/image/width=600/https://crumbl.video/84a79b26-362c-426c-b197-e9ba680497d9_CinnamonFryBread_Aerial_Tech.png',
+      description:
+        'A melt-in-your-mouth frybread cookie smothered with melted butter and topped with a delicious scoop of cinnamon buttercream.',
+    },
+    {
+      id: 5,
+      name: 'Mallow Sandwich',
+      img: 'https://crumbl.video/cdn-cgi/image/width=600/https://crumbl.video/5f4a417f-ef6a-4ee8-b7f3-dec2cab85212_MallowSandwichFtOreo_Aerial_Tech.png',
+      description:
+        'An OREO super sandwich—a fluffy, creamy center sandwiched between two chocolate brownie cookies with OREO Cookie Pieces.',
+    },
+    {
+      id: 6,
+      name: 'Circus Animal',
+      img: 'https://crumbl.video/cdn-cgi/image/width=600/https://crumbl.video/8cd90c5b-72e0-429c-94d9-daf35bd182ae_SugarFtMothersCircusAnimal_Aerial_Tech.png',
+      description:
+        'A sweet rainbow sprinkles cookie smothered with melty white chips then topped with a Circus Animal Cookie and a splash of rainbow sprinkles.',
+    },
+  ],
+  iceCream: [
+    {
+      id: 1,
+      name: 'Vanilla Bean',
+      img: 'https://crumbl.video/cdn-cgi/image/width=600/https://crumbl.video/f41d8520-caf9-4d97-8746-2496030e34c0_VanillaBean_TubWithScoops_InStoreMenu_Transparent.png',
+      description: 'Classic vanilla. Rich and creamy.',
+    },
+    {
+      id: 2,
+      name: 'Cookie Dough Crunch',
+      img: 'https://crumbl.video/cdn-cgi/image/width=600/https://crumbl.video/dfc1d8bc-bea0-4957-a2b6-e0070ac80d5d_CookieDoughCrunch_TubWithScoops_InStoreMenu_Transparent.png',
+      description:
+        'Rich vanilla mixed with cookie dough lumps and dark chocolate chips.',
+    },
+    {
+      id: 3,
+      name: 'Snickerdoodle',
+      img: 'https://crumbl.video/cdn-cgi/image/width=600/https://crumbl.video/b07847e6-6436-4902-b353-161ca05e4b36_Snickerdoodle_TubWithScoops_InStoreMenu_Transparent.png',
+      description:
+        'Cinnamon vanilla ice cream mixed with half-baked Snickerdoodle cookie bits.',
+    },
+    {
+      id: 4,
+      name: 'Raspberry Cheesecake',
+      img: 'https://crumbl.video/cdn-cgi/image/width=600/https://crumbl.video/d2bebf90-84a6-4dc1-9766-e84b4799afa7_RaspberryCheesecake_TubWithScoops_InStoreMenu_Transparent.png',
+      description:
+        'French vanilla bean ice cream with raspberry swirl and cheesecake pieces.',
+    },
+    {
+      id: 5,
+      name: 'Brownie Fudge',
+      img: 'https://crumbl.video/cdn-cgi/image/width=600/https://crumbl.video/14ae2f12-91a6-4aec-9d5a-b412e597af61_BrownieFudge_TubWithScoops_InStoreMenu_Transparent.png',
+      description: 'Creamy chocolate with brownie chunks.',
+    },
+    {
+      id: 6,
+      name: 'Peanut Butter Brittle',
+      img: 'https://crumbl.video/cdn-cgi/image/width=600/https://crumbl.video/29a77071-f788-4b5c-a1b0-3f48e0608402_PeanutButterBrittle_TubWithScoops_InStoreMenu_Transparent.png',
+      description: 'Peanut butter ice cream and some peanut brittle crunch.',
+    },
+  ],
+}
